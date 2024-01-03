4884cb85a87d907edfce8a9f8658689d5f0098ec
kobapi28 <sho1228.k@gmail.com>
2024-01-03 21:47:29 +0900

    feat: syntax highlight があたるように

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,18 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  markdown: {
+    shikiConfig: {
+      // Shikiの組み込みテーマから選択（または独自のテーマを追加）
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // カスタム言語の追加
+      // 注：Shikiには.astroを含む無数の言語が内蔵されています。
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // 水平スクロールを防ぐために文字の折り返しを有効にする
+      wrap: true,
+    },
+  },
 });
