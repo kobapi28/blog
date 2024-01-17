f1ca102359b2308f711217410cd3d86960c581de
kobapi28 <sho1228.k@gmail.com>
2024-01-17 22:39:52 +0900

    fix: 中央に揃えた

--- src/components/PostItem/index.tsx
+++ src/components/PostItem/index.tsx
@@ -1,19 +1,23 @@
 import type { FC } from "preact/compat"
 import Tag from "../Tag"
 import s from './style.module.css'
+import c from 'clsx'
 
 type Props = {
   title: string
   date: string
   tags: string[]
   url: string
+  description: string
+  className?: string
 }
 
-const PostItem: FC<Props> = ({ title, date, tags, url }) => {
+const PostItem: FC<Props> = ({ title, date, tags, url, description, className }) => {
   return (
-    <article className={s.root}>
+    <article className={c(s.root, className)}>
       <a href={url} className={s.link}/>
       <h2 className={s.title}>{title}</h2>
+      <p>{description}</p>
       <ul className={s.tags}>
         {tags.map(tag => (
           <li>
