bb233cc04e514b56c43f56a7e3bf11788d265b62
kobapi28 <sho1228.k@gmail.com>
2024-01-28 23:00:47 +0900

    fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,10 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET() {
   return rss({
-    title: 'WIP: こばぴのブログ',
-    description: 'WIP: なんでも書くブログ',
-    site: 'https://wip.example.com',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    title: 'kobapi.me',
+    description: '技術やら日常やら',
+    site: 'https://kobapi.me',
+    items: await pagesGlobToRssItems(import.meta.glob<import('@/types/frontMatter').FrontMatter>('./**/*.md')),
     customData: `<language>ja-jp</language>`,
   });
 }
---
+++ src/types/frontMatter.ts
@@ -0,0 +1,10 @@
+export interface FrontMatter {
+  title: string;
+  description: string;
+  publishedAt: string;
+  tags: string[];
+  image: {
+    url: string;
+    alt: string;
+  }
+}
