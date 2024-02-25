4f0b853296dec82af0de9fd320de7cb90efba154
kobapi28 <sho1228.k@gmail.com>
2024-02-25 22:03:46 +0900

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   markdown: {
     shikiConfig: {
-      // Shikiの組み込みテーマから選択（または独自のテーマを追加）
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      /*
+       * Shikiの組み込みテーマから選択（または独自のテーマを追加）
+       * https://github.com/shikijs/shiki/blob/main/docs/themes.md
+       */
       theme: 'dracula',
-      // カスタム言語の追加
-      // 注：Shikiには.astroを含む無数の言語が内蔵されています。
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      /*
+       * カスタム言語の追加
+       * 注：Shikiには.astroを含む無数の言語が内蔵されています。
+       * https://github.com/shikijs/shiki/blob/main/docs/languages.md
+       */
       langs: [],
       // 水平スクロールを防ぐために文字の折り返しを有効にする
       wrap: true
     }
   }
-});
+})
